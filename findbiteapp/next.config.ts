741171a1b1f5f4bbd1dea18924665c6fb9da6568
Subramanian <nagusubra@gmail.com>
2025-05-11 09:37:32 -0600

    v3.0 - added code

--- findbiteapp/next.config.ts
+++ findbiteapp/next.config.ts
@@ -2,12 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   images: {
     domains: ['unxogilsnswceszwrbng.supabase.co'],
-  },
-  experimental: {
-    serverActions: true,
+  },  experimental: {
+    serverActions: {
+      bodySizeLimit: '16mb'
+    }
   }
 };
 
